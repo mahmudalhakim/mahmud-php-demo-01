ff6513e8652c7d87b47c55430e4c93e5dc831341
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2021-04-13 08:39:23 +0200

    Heroku MySQL Connect

--- db.php
+++ db.php
@@ -5,16 +5,24 @@
  *       med PDO (PHP Data Object)
  **********************************************/
 
+/*
 $db_servname = "localhost";
-$db_servport = "80";
 $db_database = "filmdb"; 
 $db_username = "root";
 $db_password = "root";
+*/
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$db_servname = $url["host"];
+$db_username = $url["user"];
+$db_password = $url["pass"];
+$db_database = substr($url["path"], 1);
+
 
 try {
     $db = new PDO("mysql:host=$db_servname;
                    dbname=$db_database;
-                   port=$db_servport;
                    charset=utf8", 
                    $db_username, 
                    $db_password);
